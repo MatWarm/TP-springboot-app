981585b6af15fae31cc771424f1affd6f9bf7d68
MattéoWarmée <mttwarmee@gmail.com>
2024-03-08 16:05:42 +0100

    correction bug test

--- src/test/java/com/example/springbootapp/SpringbootAppApplicationTests.java
+++ src/test/java/com/example/springbootapp/SpringbootAppApplicationTests.java
@@ -3,6 +3,8 @@ package com.example.springbootapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Fail.fail;
+
 @SpringBootTest
 class SpringbootAppApplicationTests {
 
